924fb0094e16936c68ebf914957cf2fcd563cd23
Harshavardhan <harshavardhanponduru@gmail.com>
2023-11-11 13:05:22 +0530

    Add files via upload
    Updated

---
+++ 2341008007/Assignment1/src/Q10.java
@@ -0,0 +1,13 @@
+
+public class Q10 {
+	public static void main(String [] args){
+	String a = "1";
+	String b = a + " " + "2" + " " + a;
+	String c = b + " " + "3" + " " + b;
+	String d = c + " " + "4" + " " + c;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	}
+}
---
+++ 2341008007/Assignment1/src/Q5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Q5 {
+	public static void main(String [] args)
+	{
+		Scanner hv = new Scanner(System.in);
+		System.out.println("PLEASE ENTER YOUR NAME");
+		String name = hv.nextLine();
+		String greeting = ", Good Afternoon";
+		System.out.print("Hey " + name + greeting);
+	}
+
+}
