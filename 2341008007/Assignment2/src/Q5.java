da878ea953c77ce4f8a961ccbb1cb1374da41111
Harshavardhan <harshavardhanponduru@gmail.com>
2023-11-13 11:57:12 +0530

    Update Q5.java

--- 2341008007/Assignment2/src/Q5.java
+++ 2341008007/Assignment2/src/Q5.java
@@ -4,7 +4,7 @@ public class Q5
     public static void main(String [] args)
     {
         Scanner hv = new Scanner(System.in);
-        System.out.println("Enter the radius of the hemisphere: ");
+        System.out.print("Enter the radius of the hemisphere: ");
         double radius = hv.nextInt();
         double surface_area = 3 * Math.PI * Math.pow(radius,2);
         double volume = ((double) 2 /3) * Math.PI * Math.pow(radius,3);
