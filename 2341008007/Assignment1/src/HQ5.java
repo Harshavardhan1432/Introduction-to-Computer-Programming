c3fe2b0d5110af69539019ae366f976b94dd53e4
Harshavardhan <harshavardhanponduru@gmail.com>
2023-11-17 16:43:24 +0530

    Add files via upload

---
+++ 2341008007/Assignment1/src/HQ5.java
@@ -0,0 +1,23 @@
+public class HQ5
+{
+        public static void main(String[] args) {
+            int W = 1, G = 2, K = 3, A = 4;
+
+            W = W + G;
+            G = W - G;
+            W = W - G;
+
+            G = G + K;
+            K = G - K;
+            G = G - K;
+
+            K = K + A;
+            A = K - A;
+            K = K - A;
+
+            System.out.println(W);
+            System.out.println(G);
+            System.out.println(K);
+            System.out.println(A);
+        }
+}
