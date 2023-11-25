7e54e6c28305263296350c4522e38c3927354407
Harshavardhan <harshavardhanponduru@gmail.com>
2023-11-25 16:17:01 +0530

    Compressed CL5

--- 2341008007/Assignment2/src/CL5.java
+++ 2341008007/Assignment2/src/CL5.java
@@ -5,20 +5,11 @@ public class CL5 {
 		int num1 = Integer.parseInt(args [0]);
 		int num2 = Integer.parseInt(args [1]);
 		int num3 = Integer.parseInt(args [2]);
-		 if (num1 > num2 && num1 > num3) {
-			 System.out.println(Math.min(num2, num3));
-			 System.out.println(Math.max(num2, num3));
-			 System.out.println(num1);
-		 }
-		 else if (num2 > num1 && num2 > num3) {
-			 System.out.println(Math.min(num1, num3));
-			 System.out.println(Math.max(num1, num3));
-			 System.out.println(num2);
-		 }
-		 else {
-			 System.out.println(Math.min(num1, num2));	
-			 System.out.println(Math.max(num1, num2));
-			 System.out.println(num3);
-		 }
+		 if (num1 > num2 && num1 > num3)
+			 System.out.println(Math.min(num2, num3) + "\n" + Math.max(num2, num3) + "\n" + num1);
+		 else if (num2 > num1 && num2 > num3)
+			 System.out.println(Math.min(num1, num3) + "\n" + Math.max(num1, num3) + "\n" + num2);
+		 else
+			 System.out.println(Math.min(num1, num2) + "\n" + Math.max(num1, num2) + "\n" + num3);
 	}
 }
